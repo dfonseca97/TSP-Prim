fa631b0b7ee43dfa99ff069442e55a2d34778aef
Diego Fonseca <dfonseca@eafit.edu.co>
2019-02-23 18:41:49 -0500

    Naming correction

--- Graph.java
+++ Graph.java
@@ -17,7 +17,7 @@ public abstract class Graph {
    }
    
    public  abstract void addArc(int source, int destination, int weight);
-   public abstract ArrayList<Integer> getSuccessors(int vertice);
+   public abstract ArrayList<Integer> getSuccessors(int vertex);
    public abstract int getWeight(int source, int destination);
    
    public  int size() {
